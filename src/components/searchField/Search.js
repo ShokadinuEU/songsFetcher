75f10fe047a2d5d4267b6a57a1a3a1657b428e4a
Timotei Dumitru <dumitru.timy@gmail.com>
2019-11-25 20:56:53 +0000

    basic functionality

--- src/components/searchField/Search.js
+++ src/components/searchField/Search.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./search.css";
+import ShowResult from "../showResult/ShowResult";
 
-export default function Input() {
+const Search = props => {
+  const YOUR_CLIENT_ID = "PGBAyVqBYXvDBjeaz3kSsHAMnr1fndq1";
+  const urlString = `https://api.soundcloud.com/tracks/13158665?client_id=${YOUR_CLIENT_ID}`;
+
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState("");
+  const [showData, setShowData] = useState(false);
+
+  async function fetchUrl() {
+    const response = await fetch(urlString);
+    const json = await response.json();
+    setData(json);
+    setUser(json.user);
+  }
+
+  useEffect(() => {
+    fetchUrl();
+  }, []);
+
+  // console.log(data, user);
+
+  const handleClick = () => {
+    setShowData(!showData);
+  };
+
   return (
     <div className="searchMain">
-      <input className="searchInput" type="text" />
-      <button className="searchButton">Go!</button>
+      <div className="inputHolder">
+        <input className="searchInput" type="text" />
+        <button className="searchButton" onClick={handleClick}>
+          Go!
+        </button>
+      </div>
+      {showData ? (
+        <ShowResult data={data} user={user} />
+      ) : (
+        <div>Looking for a song?</div>
+      )}
     </div>
   );
-}
+};
+
+export default Search;
---
+++ src/components/showResult/ShowResult.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../searchField/search.css";
+
+const ShowResult = props => {
+  const data = props.data;
+  const user = props.user.username;
+  console.log(data, user);
+
+  return (
+    <div>
+      <div className="showResult">
+        <h2>Art work is apparently "null" from api</h2>
+        <h4>Artist Name: {user}</h4>
+        <h5>Song Name: {data.title} </h5>
+        <a href={data.permalink_url}>Click here to for more Info</a>
+      </div>
+    </div>
+  );
+};
+
+export default ShowResult;
